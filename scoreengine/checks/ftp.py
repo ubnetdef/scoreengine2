33b9ea50d36ee073a42ec4329c4a230821ba1bc6
Stephen James <stephenorjames@gmail.com>
2018-11-28 01:05:23 -0500

    Update FTP check for SC2

--- scoreengine/checks/ftp.py
+++ scoreengine/checks/ftp.py
@@ -1,12 +1,8 @@
-# TODO: fix and test this module
-
-from binascii import hexlify
-from ftplib import FTP
-from os.path import basename
-from os import urandom
-from os.path import getsize
-from random import choice
-from tempfile import NamedTemporaryFile
+import binascii
+import ftplib
+import os
+import random
+import tempfile
 
 from . import config, check_function
 
@@ -15,7 +11,6 @@ from . import config, check_function
 ftp_config = {
     'timeout': 15,
     'prefix': 'scoreengine_',
-    # 'bufsize': 0,
     'directory': '',
 }
 # /DEFAULTS
@@ -28,62 +23,46 @@ if 'ftp' in config['checks']:
 
 @check_function('Successful connect, upload, and deletion of a file')
 def check_upload_download(check):
-    # Create a temp file
-    check_file = NamedTemporaryFile(prefix=ftp_config['prefix'])
-
-    # Write random amount of bytes to the check_file
-    # Size should be 2x random_bytes due to hexlify
-    random_bytes = choice(range(1000, 9000))
-    check_file.write(hexlify(urandom(random_bytes)))
-    check_file.seek(0)
-
-    check_file_name = ftp_config['directory'] + basename(check_file.name)
-    check_file_size = getsize(check_file.name)
-    ftp = None
-
-    check.add_output('Starting check...')
-
-    try:
-        # Start the connection
-        check.add_output('Connecting to {HOST}...', **check.config)
-        ftp = FTP(check.config['HOST'], timeout=ftp_config['timeout'])
+    check.add_output('Connecting to {HOST}...', **check.config)
+    with ftplib.FTP(check.config['HOST'], timeout=ftp_config['timeout']) as ftp:
         check.add_output('Connected!')
 
-        # Login
-        check.add_output('Attempting to login as {USER}', **check.config)
+        # Log in
+        check.add_output('Attempting to log in as {USER}', **check.config)
         ftp.login(check.config['USER'], check.config['PASS'])
         check.add_output('Authentication successful!')
 
-        # Attempt to upload a file
+        # Create a temporary file with random data for uploading
+        check_file = tempfile.NamedTemporaryFile(prefix=ftp_config['prefix'])
+        number_of_bytes = random.randint(1000, 9000)
+        binary_data = os.urandom(number_of_bytes)
+        ascii_data = binascii.hexlify(binary_data)  # twice the size of binary_data
+        check_file.write(ascii_data)
+        check_file.seek(0)
+        check_file_name = ftp_config['directory'] + os.path.basename(check_file.name)
+        check_file_size = os.path.getsize(check_file.name)
+
+        # Attempt to upload the file
         ftp.cwd(ftp_config['directory'])
         check.add_output('Uploading file {} with {} bytes...', check_file_name, check_file_size)
         ftp.storbinary('STOR ' + check_file_name, check_file)
         check.add_output('Uploaded!')
 
-        # Get the size of the file
-        check.add_output('Getting size of {}....'.format(check_file_name))
-        actual_size = ftp.size(check_file_name)
-        if actual_size != check_file_size:
-            check.add_output('File size is {}, not the same as source ({})! Failure!'.format(actual_size, check_file_size))
-
-            ftp.close()
-            return
+        # Validate the uploaded file
+        check.add_output("Getting size of {}....".format(check_file_name))
+        actual_file_size = ftp.size(check_file_name)
+        if actual_file_size != check_file_size:
+            check.add_output(
+                'File size is {}, not the same as source ({})! Failure!',
+                actual_file_size,
+                check_file_size,
+            )
+            return False
         check.add_output('File size check passed!')
 
-        # Delete it
-        check.add_output('Deleting file {}...', check_file_name)
+        # Delete the uploaded file
+        check.add_output("Deleting file {}...".format(check_file_name))
         ftp.delete(check_file_name)
-        check.add_output('Deleted!')
+        check.add_output("Deleted!")
 
-        # Passed!
-        ftp.close()
-
-        check.setPassed()
-        check.add_output('Check successful!')
-    except Exception as e:
-        check.add_output('ERROR: {}: {}'.format(type(e).__name__, e))
-
-        if ftp is not None:
-            ftp.close()
-
-        return
+    return True
